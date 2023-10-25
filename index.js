b0dfa2d4bcc884f5fa83c60dfa0003833ccd13ea
Olga <minaeva9@gmail.com>
2023-10-24 20:33:28 -0400

    add three buttons for player choice

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
-let computerChoice;
+const userChoiceDisplay = document.createElement("h1");
+const computerChoiceDisplay = document.createElement("h1");
+const resultDisplay = document.createElement("h1");
+const gameGrid = document.getElementById("game");
 
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  if (randomNumber === 1) {
-    computerChoice = "rock";
-  }
-  if (randomNumber === 2) {
-    computerChoice = "paper";
-  }
-  if (randomNumber === 3) {
-    computerChoice = "scissors";
-  }
-  console.log(computerChoice);
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
+
+const choices = ["rock", "paper", "scissors"];
+
+const userChoice = () => {
+  console.log('clicked');
 }
 
-let playerChoice = prompt("Please choose 'rock', 'paper' or 'scissors'");
-let player = playerChoice.trim().toLocaleLowerCase();
-if (player === "rock" || player === "paper" || player === "scissors") {
-  console.log(player);
-  getComputerChoice();
-} else {
-  alert("You did not enter correct");
-}
-
-function playRound() {
-  if (computerChoice === player) {
-    console.log("It is a Draw");
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement('button');
+  button.id = choices[i];
+  button.innerHTML = choices[i];
+  button.addEventListener('click', userChoice);
+  gameGrid.appendChild(button);
   }
-  if (computerChoice === "rock" && player === "paper") {
-    console.log("You Win");
-  }
-  if (computerChoice === "rock" && player === "scissors") {
-    console.log("You Lost");
-  }
-  if (computerChoice === "paper" && player === "scissors") {
-    console.log("You Win");
-  }
-  if (computerChoice === "paper" && player === "rock") {
-    console.log("You Lost");
-  }
-  if (computerChoice === "scissors" && player === "rock") {
-    console.log("You Win");
-  }
-  if (computerChoice === "scissors" && player === "paper") {
-    console.log("You Lost");
-  }
-}
-playRound();
